ae7502042e1b2922bddb7a38025195f29eaae717
Claudia Watson <claudia@stackhpc.com>
2026-06-30 10:29:24 +0100

    Edits to vast module

--- variables.tf
+++ variables.tf
@@ -1,11 +1,63 @@
 variable "vippools" {
   type = map(
     object({
+
+      name                  = optional(string)
+      network               = optional(string)
       vlan                  = optional(number)
       role                  = optional(string)
       subnet_cidr           = optional(number)
-      ip_ranges = optional(list(list(string)), [])
+      tenant_id             = optional(string)
+      project               = optional(string)
+      # may need renaming
+      vip_ranges            = optional(list(object({
+        subnet = string
+        start  = number
+        end    = number
+      })), [])
+      ip_ranges             = optional(list(list(string)), [])
     })
   )
   default = {}
+}
+
+variable "vast_tenants" {
+  type = map(
+    object({
+      name                 = string
+      allow_locked_users   = optional(bool, null)
+      allow_disabled_users = optional(bool, null)
+      # may need renaming
+      client_ranges        = optional(list(object({
+        subnet = string
+        start  = number
+        end    = number
+      })), [])
+      client_ip_ranges     = optional(list(list(string)), [])
+    })
+  )
+  default = {}
+}
+
+variable "subnets" {
+  type = map(object({
+    id         = string
+    gateway_ip = optional(string)
+    cidr       = optional(string)
+  }))
+}
+
+variable "projects" {
+  type = map(object({
+    id = string
+  }))
+}
+
+variable "networks" {
+  type = map(object({
+    id       = string
+    segments = list(object({
+      segmentation_id = optional(number)
+    }))
+  }))
 }
